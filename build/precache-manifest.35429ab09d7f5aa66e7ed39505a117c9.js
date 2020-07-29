feae952bdc071b78a4d82d7ae70787df4ff26c1f
anahv <ana@iairgroup.com>
2020-07-29 11:36:34 +0100

    trying to deploy to heroku

---
+++ build/precache-manifest.35429ab09d7f5aa66e7ed39505a117c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18db5162f7a540e152cd3ef54a7e508b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfd7ae50a4daff99982f",
+    "url": "/static/js/2.1af805d2.chunk.js"
+  },
+  {
+    "revision": "74e3b466e92a914eb240",
+    "url": "/static/js/main.3f844f23.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,78 +2,61 @@ import React, {useState, useEffect} from "react"
 
 function App() {
 
-  const [totalIntervals, setTotalInvervals] = useState(10)
-  const [remainingIntervals, setRemainingIntervals] = useState(10)
+  const [totalIntervals, setTotalIntervals] = useState(32)
+  const [remainingIntervals, setRemainingIntervals] = useState(totalIntervals)
+  const intervalDuration = 10
+
+  const bringAudio = new Audio("http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a")
+  const popAudio = new Audio("http://codeskulptor-demos.commondatastorage.googleapis.com/pang/pop.mp3")
 
   const Timer = ({seconds}) => {
-    // initialize timeLeft with the seconds prop
     const [timeLeft, setTimeLeft] = useState(seconds);
-    const [isActive, setIsActive] = useState(false)
+    // const [isActive, setIsActive] = useState(false)
 
-    const bringAudio = new Audio("http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a")
-    const popAudio = new Audio("http://codeskulptor-demos.commondatastorage.googleapis.com/pang/pop.mp3")
-    function toggle() {
-      setIsActive(!isActive)
-    }
-
-    useEffect(() => {
-      document.getElementById("intervalsLeft").innerHTML = `${remainingIntervals} / ${totalIntervals}`
-    })
+    // function toggle() {
+    //   setIsActive(!isActive)
+    // }
 
     useEffect(() => {
       if (!timeLeft)
         return;
 
-      // let interval = null
-
-      // save intervalId to clear the interval when the
-      // component re-renders
-      // if (isActive) {
-      const intervalId = setInterval(() => {
+      const interval = setInterval(function() {
         setTimeLeft(timeLeft - 1);
       }, 1000);
-      //   {
-      //   interval = setInterval(() => {
-      //     setTimeLeft(timeLeft - 1);
-      //   }, 1000);
-      // }
-      // else if (!isActive && timeLeft !== 0 ) {clearInterval(interval)}
 
-      // clear interval on re-render to avoid memory leaks
-      return() => clearInterval(intervalId);
-      // }
-    }, [isActive, timeLeft]);
+      return() => clearInterval(interval);
+    }, [timeLeft]);
 
-    if (!timeLeft && remainingIntervals > 0) {
-      start()
+    if (remainingIntervals === 0) {
+      if (!timeLeft) {
+        bringAudio.play()
+      } else if (timeLeft < 6) {
+        popAudio.play()
+      }
+    } else if (remainingIntervals > 0 && !timeLeft) {
       setRemainingIntervals(remainingIntervals - 1)
-      bringAudio.play();
-    } else if (timeLeft < 5 && remainingIntervals === 0) {
-      popAudio.play()
-    } else if (!timeLeft && remainingIntervals === 0) {
+      setTimeLeft(intervalDuration)
       bringAudio.play();
     }
 
     function start() {
-      setTimeLeft(30)
-      setIsActive(true)
+      setTimeLeft(intervalDuration)
     }
     function pauseTimer() {
-      toggle()
       setRemainingIntervals(0)
       setTimeLeft(0)
     }
     function restartIntervals() {
       setRemainingIntervals(totalIntervals)
-      setTimeLeft(30)
-      setIsActive(false)
+      setTimeLeft(intervalDuration)
+      // setIsActive(false)
     }
 
     return (<div>
       <div className="timer">
         <h1>{timeLeft}</h1>
       </div>
-      {/* <span id="audio"><audio preload="auto" id="audioElement" crossOrigin="anonymous" src="../../public/BeepPing.wav"></audio></span> */}
       <div className="buttons">
         <button id="start" className="button" onClick={start}>
           Start
@@ -92,23 +75,24 @@ function App() {
       <h1>Stretching Timer</h1>
     </header>
 
-    <h2 id="intervalsLeft">`${remainingIntervals}
-      / ${totalIntervals}`</h2>
-
+    <h2 id="intervalsLeft">
+      {remainingIntervals}
+      /{totalIntervals}
+    </h2>
     <div className="buttons">
       <button id="plus" onClick={x => {
-          setTotalInvervals(totalIntervals - 1)
+          setTotalIntervals(totalIntervals - 1);
         }}>
         <i className="fa fa-minus"></i>
       </button>
       <button id="plus" onClick={x => {
-          setTotalInvervals(totalIntervals + 1)
+          setTotalIntervals(totalIntervals + 1);
         }}>
         <i className="fa fa-plus"></i>
       </button>
     </div>
 
-    <Timer seconds="30"/>
+    <Timer seconds={intervalDuration}/>
 
   </div>);
 }
